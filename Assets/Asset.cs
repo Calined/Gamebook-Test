96fe63710936f8bed0625f7d27394cb29299a91a
Chris Manthey <calined@gmx.de>
2018-09-20 13:03:08 +0200

    first commit

---
+++ Assets/Asset.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Linq;
+
+[Serializable]
+public class Asset
+{
+    public string Name;
+    public string url;
+    public string type;
+
+    public virtual IEnumerator Load()
+    {
+        if (_www == null)
+        {
+            _www = new WWW(url);
+            yield return _www;
+
+            if (_www.error == null)
+            {
+                yield return new WaitForEndOfFrame();
+
+                var request = _www.assetBundle.LoadAllAssetsAsync();
+
+                yield return request;
+
+                yield return new WaitForEndOfFrame();
+                yield return new WaitForEndOfFrame();
+
+                AssetObj = request.allAssets[0];
+            }
+
+            Add();
+        }
+    }
+
+    public virtual void Add()
+    {
+        GameManager.assets.Add(this);
+    }
+
+    public WWW _www;
+    protected UnityEngine.Object AssetObj;
+
+    public virtual void Instantiate()
+    {
+        GameObject instantiatedasset = null;
+        if (type == "character")
+        {
+            instantiatedasset = GameObject.Instantiate(AssetObj, Vector3.zero, Quaternion.identity) as GameObject;
+        }
+    }
+}
+
+[System.Serializable]
+public class AudioAsset : Asset
+{
+    public AudioClip AudioClip;
+
+    public AudioClip AudioClipProperty
+    {
+        get { return AudioClip; }
+        set
+        {
+            if (AudioClip == null)
+            {
+                AudioClip = value;
+                GameManager.assets.Add(this);
+            }
+            else
+            {
+                for (int i = 0; i < GameManager.assets.Count(); ++i)
+                {
+                    if (GameManager.assets[i] == this)
+                    {
+                        Debug.LogWarning("There is already an audio asset in audioasset like this: " + AudioClip);
+                    }
+                }
+            }
+        }
+    }
+
+    public override IEnumerator Load()
+    {
+        if (_www == null)
+        {
+            _www = new WWW(url);
+            yield return _www;
+
+            if (_www.error == null)
+            {
+                yield return new WaitForEndOfFrame();
+
+                if (_www.GetAudioClip() != null)
+                {
+                    var request = _www.GetAudioClip(true);
+
+                    yield return request;
+
+                    AudioClip = request;
+                }
+            }
+
+            Add();
+        }
+    }
+
+    public override void Add()
+    {
+        base.Add();
+
+        Debug.Log("Audio loaded");
+    }
+
+    public override void Instantiate()
+    {
+        new GameObject("Audio", typeof(AudioSource));
+        AudioSource audiosource = GameObject.Find("Audio").GetComponent("AudioSource") as AudioSource;
+
+        audiosource.clip = AudioClip;
+        audiosource.Play();
+        Debug.Log("For audio assets there is nothing to instantiate!");
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    static private GameManager Instance;
+
+    public List<TextAsset> _jsonTexts;
+
+    static public List<Asset> assets;
+    static public List<GameObject> InstantiatedAssets;
+
+    public static Asset ActiveLoadingAsset;
+    public static AsyncOperation CurrentInstantiatingAssetOperation { get; internal set; }
+
+    private static bool _isSceneSetup;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        Instance = this;
+        assets = new List<Asset>();
+        InstantiatedAssets = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        foreach (var text in _jsonTexts)
+        {
+            var asset = ReadFromJson(text.text);
+
+            if (asset.type.Equals("audio"))
+                asset = ReadAudioAssetFromJson(text.text);
+
+            StartCoroutine(asset.Load());
+        }
+
+        AssetCount = 0;
+    }
+
+    public int AssetCount
+    {
+        set
+        {
+            if (assets.Count > value)
+                assets.RemoveRange(value, assets.Count - value);
+        }
+    }
+
+    private static void SetupScene()
+    {
+        if (assets.Count == ((GameManager)GameObject.Find("GameManager").GetComponent("GameManager"))._jsonTexts.Count && _isSceneSetup == false)
+        {
+            foreach (var asset in assets)
+            {
+                asset.Instantiate();
+            }
+
+            _isSceneSetup = true;
+
+            //reset
+            GameManager.Instance.AssetCount = 0;
+        }
+    }
+
+    static public Asset ReadFromJson(string JSONString)
+    {
+        return JsonUtility.FromJson<Asset>(JSONString);
+    }
+
+    private static AudioAsset ReadAudioAssetFromJson(string JSONString)
+    {
+        return JsonUtility.FromJson<AudioAsset>(JSONString);
+    }
+
+    public void Update()
+    {
+        if (GameManager.CurrentInstantiatingAssetOperation != null)
+        {
+            if (GameManager.CurrentInstantiatingAssetOperation.isDone == true)
+            {
+                if (GameManager.ActiveLoadingAsset.type.Equals("character"))
+                {
+                    Debug.Log(GameManager.ActiveLoadingAsset.Name + "has finished loading from" + GameManager.ActiveLoadingAsset.url);
+                }
+
+                if (GameManager.ActiveLoadingAsset.type.Equals("location"))
+                    Debug.Log("Location " + GameManager.ActiveLoadingAsset.Name + "has been loaded!");
+            }
+        }
+
+        SetupScene();
+    }
+}
