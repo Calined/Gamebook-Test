a53aed32f5a1d742f46e8401aa8e8259c56171c0
Chris Manthey <calined@gmx.de>
2018-09-21 13:35:42 +0200

    adding more comments

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -37,49 +37,66 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        //for each text in the text list of text assets 
         foreach (var text in _jsonTexts)
         {
+            //assign the content to asset
             var asset = ReadFromJson(text.text);
 
+            //if the asset type is "audio"
             if (asset.type.Equals("audio"))
+                //read it as an audio asset
                 asset = ReadAudioAssetFromJson(text.text);
-
+            //start the asset load as a coroutine
             StartCoroutine(asset.Load());
         }
 
+        //some public int field with an overwritten set method
         AssetCount = 0;
     }
 
+    //public field
     public int AssetCount
     {
+        //overwritten setter
         set
         {
+            //if the count of assets is higher than the value that is coming in
             if (assets.Count > value)
+                //remove assets in that range from the list
+                //from the back of it but also huh?
                 assets.RemoveRange(value, assets.Count - value);
         }
     }
 
+    //private method to set up the scene
     private static void SetupScene()
     {
+        //if the assets.count is the same as the json text assets count and the scene is not set up yet
         if (assets.Count == ((GameManager)GameObject.Find("GameManager").GetComponent("GameManager"))._jsonTexts.Count && _isSceneSetup == false)
         {
+            //for each asset in assets
             foreach (var asset in assets)
             {
+                //instantiate it
                 asset.Instantiate();
             }
-
+            //after that set the scene to set up
             _isSceneSetup = true;
 
             //reset
+            //and then set the asset counter to 0 again??
             GameManager.Instance.AssetCount = 0;
         }
     }
 
+    //read the asset from json
     static public Asset ReadFromJson(string JSONString)
     {
         return JsonUtility.FromJson<Asset>(JSONString);
     }
 
+    //read the audio asset from json
     private static AudioAsset ReadAudioAssetFromJson(string JSONString)
     {
         return JsonUtility.FromJson<AudioAsset>(JSONString);
@@ -87,20 +104,25 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
+        //if the operation is not null, so probably if its not instantiating right now
         if (GameManager.CurrentInstantiatingAssetOperation != null)
         {
+            //if its done
             if (GameManager.CurrentInstantiatingAssetOperation.isDone == true)
             {
+                //if the active loading type is a character
                 if (GameManager.ActiveLoadingAsset.type.Equals("character"))
                 {
+                    //print that to log
                     Debug.Log(GameManager.ActiveLoadingAsset.Name + "has finished loading from" + GameManager.ActiveLoadingAsset.url);
                 }
-
+                //if its a location
                 if (GameManager.ActiveLoadingAsset.type.Equals("location"))
+                    //print that to the log
                     Debug.Log("Location " + GameManager.ActiveLoadingAsset.Name + "has been loaded!");
             }
         }
-
+        //set up the scene
         SetupScene();
     }
 }
